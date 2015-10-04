42d7e2b2e8d12dca723fffeb55425c586ce4f66d
aec1140 <aec1140@rit.edu>
2015-10-04 16:43:07 -0400

    Seek code is finished. Unable to set-up firing with the enemy due to fire code rotating the vehicle and firing. I suggest rotating the vehicle in a separate method and fire just fires in the transform.forward direction.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+public class Enemy : Vehicle {
+
+	public Vehicle target;
+	public Vector3 desired;
+
+	// Use this for initialization
+	public void Start()
+	{
+		base.Start();
+		friendly = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		base.Update();
+		Move (Seek ());
+
+		// Fire at the player
+
+	}
+
+	// Seek the target
+	Vector3 Seek(){
+		// vector between the target and the enemy
+		desired = target.transform.position - this.transform.position;
+
+		// nullify y translation
+		desired.y = 0;
+
+		// determine smaller axis to travel
+		if (Math.Abs (desired.x) > Math.Abs (desired.z)) {
+			desired.z = 0;
+			return desired;
+		} 
+		else {
+			desired.x = 0;
+			return desired;
+		}
+	}
+}
