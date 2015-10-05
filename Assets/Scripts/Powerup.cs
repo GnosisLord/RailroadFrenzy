85a33e311d2ef7c260b47cd4eb50d4cc067fc186
unknown <awh5426@GDD-20.main.ad.rit.edu>
2015-10-04 20:13:08 -0400

    Added powerup script so that the player can pick it up and it increases the player's size. Also, moved the models to the middle so we'd have more room, and the enemy's tracking seems to have broken quite badly.

---
+++ Assets/Scripts/Powerup.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Powerup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Triggers on collision with object
+	void OnTriggerEnter(Collider other)
+	{
+		//scale up object
+		other.gameObject.transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
+
+		//remove this powerup
+		Destroy (this.gameObject);
+	}
+}
